887acde76f0a4596d98f3dcfa257d6214cf27ee3
O Barron <otsobnalle@gmail.com>
2020-01-08 18:42:47 -0800

    Smoother compensation

--- static/js/clientcontroller.js
+++ static/js/clientcontroller.js
@@ -29,7 +29,11 @@ export default class ClientObjectController {
         this.unauthorized_inputs = [];
         this.input_number = 0;
 
-        this.speed = 50;
+        this.speed = 75;
+
+        this.adjust_x = 0;
+        this.adjust_y = 0;
+        this.adjusting = false;
         /* some more commands to know
 		//app.stage.removeChild(anySprite)
 		//anySprite.visible = false;
@@ -46,6 +50,8 @@ export default class ClientObjectController {
     }
 
     processInputs(dt){
+        //not only process inputs, but interpolate me a bit
+
         console.log(`There are ${this.unauthorized_inputs.length} unauthorized inputs`)
 
         if(this.entity == null){
@@ -71,7 +77,37 @@ export default class ClientObjectController {
     reconcile(entity_state, verified_num){
         // discard all the inputs that have been implicitly verified on the server,
         // because they are less than or equal to this one (websockets guarentees order)
+        if(this.adjusting){
+            let maxtween_x = .1 * this.adjust_x;
+            let maxtween_y = .1 * this.adjust_y;
+
+            let cx = this.entity.getX();
+            let cy = this.entity.getY();
+    
+            let comp_x = Math.sign(this.adjust_x) * Math.min(Math.abs(maxtween_x), Math.abs(this.adjust_x))
+            let comp_y = Math.sign(this.adjust_y) * Math.min(Math.abs(maxtween_y), Math.abs(this.adjust_y))
+           
+            this.adjust_x -= comp_x;
+            this.adjust_y -= comp_y;
+
+            cx += this.comp_x;
+            cy += this.comp_y;
+
+            if(this.adjust_x == 0 && this.adjust_y == 0){
+                this.adjusting = false;
+            }
+    
+            this.entity.setPosition(cx,cy);
+        }
+
+
         if(this.unauthorized_inputs.length == 0){
+            return;
+        }
+
+        //if our first one is greater than the num, that means we have already discarded it.. 
+        //which happens when we snap back
+        if(this.unauthorized_inputs[0][0] > verified_num){
             return;
         }
         
@@ -81,28 +117,51 @@ export default class ClientObjectController {
         // go through the unauthorized_inputs and apply them to get a more accurate position
         // only if we are a substantial distance from the server state
 
-        // at this point, 0 index should = the last unnauthored input
+        // at this point, 0 index should = the verified_num
 
         if(this.unauthorized_inputs.length == 0){
             return;
         }
 
-        const distance = Math.sqrt((Math.pow(this.unauthorized_inputs[0][2] - entity_state["x"],2) + (Math.pow(this.unauthorized_inputs[0][3] - entity_state["y"],2))))
+        const _x = Math.round(this.unauthorized_inputs[0][2])
+        const _y = Math.round(this.unauthorized_inputs[0][3])
 
-        if(distance > 10){
+        const distance = Math.sqrt(Math.pow(_x - entity_state["x"],2) + (Math.pow(_y - entity_state["y"],2)))
+
+        //if get to far away , , , snap back
+        if(distance > 40){
+            // snap reconciliation
+            console.log("SNAP RECONCILIATION");
+            this.entity.setPosition(entity_state["x"],entity_state["y"]);
+
+            this.unauthorized_inputs = [];
+            // clears this stuff
+
+            this.adjusting = false;
+            this.adjust_x = 0;
+            this.adjust_y = 0;
+
+            return;
+        }
+
+        if(distance > 10 && !this.adjusting){
+            this.adjust_x = entity_state["x"] - _x; // how much we need to move to be at our desired location
+            this.adjust_x = entity_state["y"] - _y; //if actualy is greater the current, its pos...
+            this.adjusting = true;
+            // OLD CODE BELOW
+            /*
             this.entity.setPosition(entity_state["x"],entity_state["y"])
             
             this.unauthorized_inputs.shift();
-
 
             let i;
             for(i = 0; i < this.unauthorized_inputs.length; i++){
                 this.applyInput(this.unauthorized_inputs[i][1]);
             }
+            */
         }
     }
 
-    //this is client side prediction for movement
     applyInput(input){
         if(this.entity == null){
             return;
