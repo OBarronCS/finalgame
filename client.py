5b9b1e72fb5a7ecb23abc578ac12ed0c3f8bea91
O Barron <otsobnalle@gmail.com>
2020-01-09 17:11:07 -0800

    Corrected bug in serverside movement smoothin

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+from math import copysign
+
 class Client:
     def __init__(self, maxInputs, player_id, sid):
         # unique ID to identify player ((length of things))
@@ -16,29 +18,34 @@ class Client:
         self.next_move = [0,0]
         
     def processInput(self, tick):
-
         self.move[0] += self.next_move[0];
         self.move[1] += self.next_move[1];
         
         self.next_move = [0,0];
 
+        
         # checks for the overflow amount of inputs
         # max amount of inputs is defined as  !!!!!  60 /  game.tickrate    !!!!
 
         i = 0;
         while(i < 2):
+            # can be negative
             inputNum = self.move[i];
-            overFlow = inputNum - self.maxInputs;
+            sign = copysign(1,inputNum);
 
+            overFlow = abs(inputNum) - self.maxInputs;
+         
             # if we have more inputs than the maximum possible
             if(overFlow > 0):
-                self.move[i] -= overFlow; # truncates it to max amount
+                # print("OVERFLOW")
+                self.move[i] -= (sign * overFlow); # truncates it to max amount
                 # design choice, +=. just = would punish people with very inconsistent output of inputs
-                self.next_move[i] += overFlow;
+                self.next_move[i] += (sign * overFlow);
 
             i += 1;
 
 
+        # print(f"{self.move[0],self.move[1]} | Overflow --> {self.next_move[0]},{self.next_move[1]}")
         self.entity.applyInput(self.move)
         self.last_verified_input = self.last_received_input;
 
